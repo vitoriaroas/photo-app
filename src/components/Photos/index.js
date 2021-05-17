2b00d0c599b1bfd26807e4fdde6b5215bd0af464
vitoriaroas <vitoriaroas@gmail.com>
2021-05-17 11:48:54 -0400

    allow user to select own photo and roatete as needed it

--- src/components/Photos/index.js
+++ src/components/Photos/index.js
@@ -1,21 +1,51 @@
-import React, { useState, useCallback } from 'react'
-import Cropper from 'react-easy-crop'
+import React, { useState, useCallback } from 'react';
+import Cropper from 'react-easy-crop';
+import { getOrientation } from 'get-orientation/browser';
+import { readFile, getRotatedImage } from '../../utils/images'
+
+const ORIENTATION_TO_ANGLE = {
+  '3': 180,
+  '6': 90,
+  '8': -90,
+};
 
 function Photo() {
-    const [crop, setCrop] = useState({ x: 0, y: 0 })
-    const [zoom, setZoom] = useState(1)
-    return (
-        <div>
-            <Cropper 
-            crop={crop}
-            zoom={zoom}
-            onCropChange={setCrop}
-            onZoomChange={setZoom}
-            image="https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000"
-            aspect={4/3}
-            />
-        </div>
-    )
+  const [imageSrc, setImageSrc] = useState(null);
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [zoom, setZoom] = useState(1);
+
+  const onFileChange = async (e) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0];
+      let imageDataUrl = await readFile(file);
+
+      // apply rotation if needed
+      const orientation = await getOrientation(file);
+      const rotation = ORIENTATION_TO_ANGLE[orientation];
+      if (rotation) {
+        imageDataUrl = await getRotatedImage(imageDataUrl, rotation);
+      }
+
+      setImageSrc(imageDataUrl);
+    }
+  };
+
+  return (
+    <div>
+      {!imageSrc ? (
+        <input type="file" accept="image/*" onChange={onFileChange} />
+      ) : (
+        <Cropper
+          crop={crop}
+          zoom={zoom}
+          onCropChange={setCrop}
+          onZoomChange={setZoom}
+          image={imageSrc}
+          aspect={4 / 3}
+        />
+      )}
+    </div>
+  );
 }
 
-export default Photo
+export default Photo;
---
+++ src/utils/images.js
@@ -0,0 +1,45 @@
+export function readFile(file) {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => resolve(reader.result), false);
+    reader.readAsDataURL(file);
+  });
+}
+
+const createImage = (url) =>
+  new Promise((resolve, reject) => {
+    const image = new Image();
+    image.addEventListener('load', () => resolve(image));
+    image.addEventListener('error', (error) => reject(error));
+    image.setAttribute('crossOrigin', 'anonymous'); // needed to avoid cross-origin issues on CodeSandbox
+    image.src = url;
+  });
+
+export async function getRotatedImage(imageSrc, rotation = 0) {
+  const image = await createImage(imageSrc);
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+
+  const orientationChanged =
+    rotation === 90 ||
+    rotation === -90 ||
+    rotation === 270 ||
+    rotation === -270;
+  if (orientationChanged) {
+    canvas.width = image.height;
+    canvas.height = image.width;
+  } else {
+    canvas.width = image.width;
+    canvas.height = image.height;
+  }
+
+  ctx.translate(canvas.width / 2, canvas.height / 2);
+  ctx.rotate((rotation * Math.PI) / 180);
+  ctx.drawImage(image, -image.width / 2, -image.height / 2);
+
+  return new Promise((resolve) => {
+    canvas.toBlob((file) => {
+      resolve(URL.createObjectURL(file));
+    }, 'image/png');
+  });
+}
